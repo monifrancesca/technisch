e3c1f552d904f1d4610166775ddbcb818b6eee1c
Moni Francesca <mo.francesca@gmail.com>
2016-04-03 23:36:54 -0500

    connecting to heroku database

--- modules/connection.js
+++ modules/connection.js
@@ -1,9 +1,11 @@
 var connectionString = '';
 
 if(process.env.DATABASE_URL != undefined) {
-  connectionString = process.env.DATABASE_URL + 'ssl';
+  connectionString = process.env.DATABASE_URL + "?ssl=true";
 } else {
   connectionString = 'postgres://localhost:5432/technisch';
 }
 
 module.exports = connectionString;
+
+
